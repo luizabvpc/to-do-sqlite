eca5692d384eb3af01003d602b9215f9007ddcf4
claud <claud@LAPTOP-SDFG0PBL>
2025-06-03 08:56:58 -0300

    quase pronto

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import { useEffect, useState } from 'react';
 import _tarefa from './types/tarefa';
@@ -54,9 +54,11 @@ export default function App() {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput style={styles.input} value={novaTarefa} onChangeText={setNovaTarefa} />
-      <Button onPress={adicionar} title='Adicionar'/>
+      <TouchableOpacity onPress={adicionar} style={styles.botao}>
+        <Text style={styles.textoBotao}>Adicionar</Text>
+      </TouchableOpacity>
       <View>
         {renderLista()}
       </View>
@@ -67,5 +69,32 @@ export default function App() {
 const styles = StyleSheet.create({
   input:{
     borderWidth: 1,
+    alignSelf: 'center',
+    width: '80%',
+    borderRadius: 10,
+    backgroundColor: '#e0f5ee',
+    borderColor: '#66cdaa',
+    color: '#1f493d',
+  },
+  container:{
+    flex: 1,
+    paddingTop: 350,
+    backgroundColor: '#fedcd3',
+  },
+  
+  botao:{
+    backgroundColor: '#66cdaa',
+    paddingVertical: '5%',
+    paddingLeft: 10,
+    width:'60%',
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: '5%',
+     alignSelf: 'center', 
+  },
+  textoBotao:{
+    color: '#007667',
+    fontWeight: 'bold',
+    fontSize: 15
   }
 });
--- components/Tarefa.tsx
+++ components/Tarefa.tsx
@@ -1,6 +1,6 @@
 import { SQLiteDatabase } from "expo-sqlite"
 import _tarefa from "../types/tarefa"
-import { Button, Text, View } from "react-native"
+import { Button, Text, View, StyleSheet, TouchableOpacity, TextComponent } from "react-native"
 
 type _propsTarefa = {
     dados: _tarefa,
@@ -22,13 +22,61 @@ export default function Tarefa(props: _propsTarefa) {
 
     const renderStatus = () => {
         if (props.dados.concluido)
-            return <Text>Concluído</Text>;
-        return <Button title="Concluir" onPress={concluir} />;
+            return <Text style={styles.textoConcluido}>✓</Text>;
+        return <TouchableOpacity onPress={concluir} style={styles.botaoConcluir}>
+                    <Text style={styles.textoBotaoConcluir}>Concluir</Text>
+                </TouchableOpacity>;
     }
 
     return <View>
-        <Text>{props.dados.texto}</Text>
+        <Text style={styles.textoTarefa}>{props.dados.texto}</Text>
         {renderStatus()}
-        <Button title="Excluir" onPress={excluir} />
+        <TouchableOpacity onPress={excluir} style={styles.botaoExcluir}>
+            <Text style={styles.textoBotaoExcluir}>Excluir</Text>
+        </TouchableOpacity>;
     </View>;
 }
+const styles = StyleSheet.create({
+    botaoConcluir:{
+        backgroundColor: '#fce083',
+        paddingVertical: '5%',
+        paddingLeft: 10,
+        width:'60%',
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: '5%',
+        alignSelf: 'center', 
+    },
+  textoBotaoConcluir:{
+        color: '#d49708',
+        fontWeight: 'bold',
+        fontSize: 15
+  },
+  textoConcluido:{
+     alignSelf: 'center', 
+     backgroundColor: '#028900',
+     color: '#bada55',
+     width: '10%',
+     textAlign: 'center',
+     borderRadius: 5,
+     fontWeight: 'bold',
+  },
+   botaoExcluir:{
+        backgroundColor: '#b787cc',
+        paddingVertical: '5%',
+        paddingLeft: 10,
+        width:'60%',
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: '5%',
+        alignSelf: 'center', 
+    },
+  textoBotaoExcluir:{
+        color: '#660066',
+        fontWeight: 'bold',
+        fontSize: 15
+  },
+  textoTarefa:{
+    alignSelf: 'center',
+  }
+});
